076906ebdc098bd68b8cf87d9100af683780450d
Michał Dwernicki <dwernicki.michal@gmail.com>
2023-10-03 13:35:42 +0200

    Added PersonsLoader

---
+++ lib/bloc/bloc_actions.dart
@@ -0,0 +1,23 @@
+import 'package:first_block_app/bloc/person.dart';
+import 'package:flutter/material.dart' show immutable;
+
+const persons1Url = 'http://127.0.0.1:5500/api/persons1.json';
+const persons2Url = 'http://127.0.0.1:5500/api/persons2.json';
+
+typedef PersonsLoader = Future<Iterable<Person>> Function(String url);
+
+@immutable
+abstract class LoadAction {
+  const LoadAction();
+}
+
+@immutable
+class LoadPersonsAction implements LoadAction {
+  final String url;
+  final PersonsLoader loader;
+
+  const LoadPersonsAction({
+    required this.url,
+    required this.loader,
+  }) : super();
+}
---
+++ lib/bloc/person.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart' show immutable;
+
+@immutable
+class Person {
+  final String name;
+  final int age;
+
+  const Person({
+    required this.name,
+    required this.age,
+  });
+
+  Person.fromJason(Map<String, dynamic> json)
+      : name = json['name'] as String,
+        age = json['age'] as int;
+
+  @override
+  String toString() => 'Person (name = $name, age = $age)';
+}
